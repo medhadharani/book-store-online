569e1ababf8728d79d1dc0b363a80df0cf66c758
Medha Dharani <medhadarani96@gmail.com>
2024-08-06 01:56:05 +0530

    forth commit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -13,7 +14,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user
+  .createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
@@ -22,9 +24,11 @@ exports.postAddProduct = (req, res, next) => {
   .then(result => {
     // console.log(result);
     console.log("Created Product");
-  }).catch(err => {
-    console.log(err)
+    res.redirect('/admin/products');
   })
+  .catch(err => {
+    console.log(err)
+  });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -33,8 +37,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(product => {
+  req.user
+  .getProducts({where: {id: prodId}})
+  // Product.findByPk(prodId)
+  .then(products => {
+    const product = products[0];
     if(!product) {
       return res.redirect('/');
     }
@@ -74,7 +81,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+  getProducts()
   .then(products => {
     res.render('admin/products', {
       prods: products,
@@ -89,6 +97,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product => {
+    return product.destroy();
+  })
+  .then(result => {
+    console.log('PRODUCT DESTROYED');
+    res.redirect('/admin/products');
+  })
+  .catch(err => {
+    console.log(err);
+  });
 }
